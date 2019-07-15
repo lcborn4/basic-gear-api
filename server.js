2d48c2c18d279e310ccfa3737713244ff2e056de
Logan Born <lcborn4@gmail.com>
2019-07-14 20:57:58 -0400

    added hello wordl

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ var express = require("express");
 var app = express();
 var items = require('./data/items').items
 
+app.get('/', function (req, res) {
+    res.send("Hello World");
+});
+
 app.get("/url", (req, res, next) => {
     console.log('items', items)
     res.json(["Tony", "Lisa", "Michael", "Ginger", "Food"]);
