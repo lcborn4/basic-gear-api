9c7d786923ee04607a145fb0c0feceeb172460a1
Logan Born <lcborn4@gmail.com>
2019-07-14 21:04:47 -0400

    removed the heroku

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ app.get("/url", (req, res, next) => {
     res.json(["Tony", "Lisa", "Michael", "Ginger", "Food"]);
 });
 
-
 app.listen(3000, () => {
     console.log("Server running on port 3000");
 });
