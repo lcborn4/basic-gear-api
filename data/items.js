476c4943af11bdb31c984d9b061fa97ca64c9f40
Logan Born <lcborn4@gmail.com>
2019-07-18 11:57:58 -0400

    Update items.js
    added the categories

--- data/items.js
+++ data/items.js
@@ -1,34 +1,157 @@
 module.exports = {
     items: [
-        //helmets
+        /*Structure*/
+        /*
+            {
+              name: string,
+              slot: string,
+              attributes: {
+                health: number,
+                attack: number,
+                defense: number,
+                mana: number,
+              }
+            }
+        */
+        
+        /*
+        Helmets
+        */
+        /* Slot: head */
+        /* Plate Helmet Armor */
         {
             name: "Steel Helmet"
         },
+ 
+        /* End of Plate Helmet Armor */
+        
+        /* Leather Helmet Armor */
         {
             name: "Ninja Mask"
         },
-        // {
-        //     name: ""
-        // },
+        /* End of Leather Helmet Armor */
+        
+        /* Cloth Helmet Armor */
+//         {
+//             name: "Cloth Ninja Mask"
+//         },
+        /* End of Cloth Helmet Armor */
 
-        //chestpieces
+        /* End of Helmets*/
+        
+        /*
+        Chest Armor
+        */
+        /* Slot: chest */
+        
+        /* Plate Chest Armor*/
         {
             name: "Steel Chestpiece"
         },
         {
             name: "Titanium Chestpiece"
         },
+        /* End of Plate Chest Armor*/
+                
+        /* Leather Chest Armor */
         {
-            name: "Titanium Chestpiece"
+            name: "Blackened Defias Armor"
         },
-        //leggings
+        /* End of Leather Chest Armor */
+        
+        /* Cloth Chest Armor */
+        {
+            name: "Robes of Insight"
+        },
+        {
+            name: 'Robes of the Royal Crown'
+        },
+        /* End of Cloth Chest Armor */
+        
+        /*
+        End of Chest Armor
+        */
+        
+        /*
+        Leggings Armor
+        */
+        /* Slot: legs */
+        
+        /* Plate Leggings Armor */
+//         {
+//             name: "Salamander Scale Pants"
+//         },
+        /* End of Plate Leggings Armor */
+        
+        /* Leather Leggings Armor */
+        {
+            name: "Salamander Scale Pants"
+        },
+        /* End of Leather Leggings Armor */
+        
+        /* Cloth Leggings Armor */
+//         {
+//             name: "Salamander Scale Pants"
+//         },
+        /* End of Cloth Leggings Armor */
 
-
-        //gauntlets
-
-
-        //boots
-
-
+        /*
+        Gauntlets
+        */
+        /* Slot: gauntlets */
+        
+        /* Plate Gauntlets Armor */
+//         {
+//             name: "Salamander Scale Pants"
+//         },
+        /* End of Plate Gauntlets Armor */
+           /* Leather Gauntlets Armor */
+//         {
+//             name: "Salamander Scale Pants"
+//         },
+        /* End of Leather Gauntlets Armor */
+        /* Cloth Gauntlets Armor */
+//         {
+//             name: "Salamander Scale Pants"
+//         },
+        /* End of Cloth Gauntlets Armor */
+        
+        /* End of Guantlets Armor */
+        /* 
+        Boots
+        */
+        /* Slot: boots */
+        
+        /* Plate Boots Armor */
+        {
+            name: "Steel Boots"
+        },
+ 
+        /* End of Plate Boots Armor */
+        
+        /* Leather Boots Armor */
+        {
+            name: "Steel Boots"
+        },
+ 
+        /* End of Leather Boots Armor */
+        
+        /* Cloth Boots Armor */
+        {
+            name: "Steel Boots"
+        },
+ 
+        /* End of Cloth Boots Armor */
+        
+        
+        /* End of Boots Armor */
+        
+        /*
+        Rings
+        */
+        /* Slot: ring */
+        
+        /* End of Rings */
+        
     ]
 }
