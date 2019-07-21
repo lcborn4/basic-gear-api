7e2f0e0c095586447372ce3688a06c8da8a03492
Logan Born <lcborn4@gmail.com>
2019-07-21 16:38:37 -0400

    leather items

---
+++ data/leather/leatherItems.js
@@ -0,0 +1,34 @@
+
+var helmets = require('./helmets')
+var chests = require('./chests')
+var legs = require('./legs')
+var boots = require('./boots')
+var gauntlets = require('./gauntlets')
+
+module.exports.leatherItems = function () {
+
+    let items = [];
+
+    helmets.forEach(element => {
+        items.push(element)
+    });
+
+    chests.forEach(element => {
+        items.push(element)
+    });
+
+    legs.forEach(element => {
+        items.push(element)
+    });
+
+    boots.forEach(element => {
+        items.push(element)
+    });
+
+    gauntlets.forEach(element => {
+        items.push(element)
+    });
+
+    return items;
+
+};
