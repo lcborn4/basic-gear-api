14b8781b0207318932c826940da492a12131f2e6
Logan Born <lcborn4@gmail.com>
2019-07-21 16:49:55 -0400

    removed newItems, replacing with items

--- data/items.js
+++ data/items.js
@@ -1,157 +1,41 @@
-module.exports = {
-    items: [
-        /*Structure*/
-        /*
-            {
-              name: string,
-              slot: string,
-              attributes: {
-                health: number,
-                attack: number,
-                defense: number,
-                mana: number,
-              }
-            }
-        */
-        
-        /*
-        Helmets
-        */
-        /* Slot: head */
-        /* Plate Helmet Armor */
-        {
-            name: "Steel Helmet"
-        },
- 
-        /* End of Plate Helmet Armor */
-        
-        /* Leather Helmet Armor */
-        {
-            name: "Ninja Mask"
-        },
-        /* End of Leather Helmet Armor */
-        
-        /* Cloth Helmet Armor */
-//         {
-//             name: "Cloth Ninja Mask"
-//         },
-        /* End of Cloth Helmet Armor */
 
-        /* End of Helmets*/
-        
-        /*
-        Chest Armor
-        */
-        /* Slot: chest */
-        
-        /* Plate Chest Armor*/
-        {
-            name: "Steel Chestpiece"
-        },
-        {
-            name: "Titanium Chestpiece"
-        },
-        /* End of Plate Chest Armor*/
-                
-        /* Leather Chest Armor */
-        {
-            name: "Blackened Defias Armor"
-        },
-        /* End of Leather Chest Armor */
-        
-        /* Cloth Chest Armor */
-        {
-            name: "Robes of Insight"
-        },
-        {
-            name: 'Robes of the Royal Crown'
-        },
-        /* End of Cloth Chest Armor */
-        
-        /*
-        End of Chest Armor
-        */
-        
-        /*
-        Leggings Armor
-        */
-        /* Slot: legs */
-        
-        /* Plate Leggings Armor */
-//         {
-//             name: "Salamander Scale Pants"
-//         },
-        /* End of Plate Leggings Armor */
-        
-        /* Leather Leggings Armor */
-        {
-            name: "Salamander Scale Pants"
-        },
-        /* End of Leather Leggings Armor */
-        
-        /* Cloth Leggings Armor */
-//         {
-//             name: "Salamander Scale Pants"
-//         },
-        /* End of Cloth Leggings Armor */
+var clothItems = require('./cloth/clothItems').clothItems
+var leatherItems = require('./leather/leatherItems').leatherItems
+var plateItems = require('./plate/plateItems').plateItems
 
-        /*
-        Gauntlets
-        */
-        /* Slot: gauntlets */
-        
-        /* Plate Gauntlets Armor */
-//         {
-//             name: "Salamander Scale Pants"
-//         },
-        /* End of Plate Gauntlets Armor */
-           /* Leather Gauntlets Armor */
-//         {
-//             name: "Salamander Scale Pants"
-//         },
-        /* End of Leather Gauntlets Armor */
-        /* Cloth Gauntlets Armor */
-//         {
-//             name: "Salamander Scale Pants"
-//         },
-        /* End of Cloth Gauntlets Armor */
-        
-        /* End of Guantlets Armor */
-        /* 
-        Boots
-        */
-        /* Slot: boots */
-        
-        /* Plate Boots Armor */
-        {
-            name: "Steel Boots"
-        },
- 
-        /* End of Plate Boots Armor */
-        
-        /* Leather Boots Armor */
-        {
-            name: "Steel Boots"
-        },
- 
-        /* End of Leather Boots Armor */
-        
-        /* Cloth Boots Armor */
-        {
-            name: "Steel Boots"
-        },
- 
-        /* End of Cloth Boots Armor */
-        
-        
-        /* End of Boots Armor */
-        
-        /*
-        Rings
-        */
-        /* Slot: ring */
-        
-        /* End of Rings */
-        
-    ]
+var rings = require('./rings/rings')
+
+module.exports.items = function () {
+
+    let items = [];
+
+    console.log('inside items')
+
+    let cloth = clothItems();
+    console.log('cloth items', cloth) //debug
+    let leather = leatherItems();
+    console.log('leather items', leather) //debug
+    let plate = plateItems();
+    console.log('plate items', plate) //debug
+
+    console.log('ring items', rings) //debug
+
+    cloth.forEach(element => {
+        items.push(element)
+    });
+
+    leather.forEach(element => {
+        items.push(element)
+    });
+
+    plate.forEach(element => {
+        items.push(element)
+    });
+
+    rings.forEach(element => {
+        items.push(element)
+    });
+
+    return items;
+
 }
